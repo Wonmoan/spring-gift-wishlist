c0efd0ad6d40ceb3e760e875c2a89fd3283e6f2c
안원모 <tommy45231@naver.com>
2024-07-03 15:01:56 +0900

    Refactor : setter 표현 변경

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,4 +1,4 @@
-package gift;
+/*package gift;
 
 import java.math.BigDecimal;
 
@@ -58,5 +58,82 @@ public class Product {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+}
+*/
+package gift;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private final Long id;
+    private final String name;
+    private final BigDecimal price;
+    private final String imageUrl;
+    private final String description;
+
+    private Product(ProductBuilder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.price = builder.price;
+        this.imageUrl = builder.imageUrl;
+        this.description = builder.description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static class ProductBuilder {
+        private Long id;
+        private String name;
+        private BigDecimal price;
+        private String imageUrl;
+        private String description;
+
+        public ProductBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public ProductBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public ProductBuilder price(BigDecimal price) {
+            this.price = price;
+            return this;
+        }
+
+        public ProductBuilder imageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+            return this;
+        }
+
+        public ProductBuilder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
     }
 }
